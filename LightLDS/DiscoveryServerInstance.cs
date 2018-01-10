d2bcecd5ae83b8863f4695fe989f715468b141a0
K47S <michi.volz@gmx.net>
2018-01-10 12:57:24 +0100

    Initial prototype

---
+++ LightLDS/DiscoveryServerInstance.cs
@@ -0,0 +1,226 @@
+﻿using Opc.Ua;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LightLDS
+{
+    public class DiscoveryServerInstance : DiscoveryServerBase
+    {
+
+        #region IDiscovery
+        public override ResponseHeader FindServers(RequestHeader requestHeader, string endpointUrl, StringCollection localeIds, StringCollection serverUris, out ApplicationDescriptionCollection servers)
+        {
+
+            var serverDummy = new ApplicationDescriptionCollection(new List<ApplicationDescription>()
+            {
+                new ApplicationDescription()
+                {
+                    ApplicationName = "SoftingOpcUaDemoServer",
+                    ApplicationUri = "opc.tcp://localhost:51510/UA/DemoServer",
+                    ApplicationType = ApplicationType.Server,
+                    ProductUri = "http://industrial.softing.com/OpcUaNetToolkit/OpcUaDemoServer",
+                    DiscoveryUrls = new StringCollection(new List<string>(){
+                            "opc.tcp://localhost:51510/UA/DemoServer"
+                    }
+                    ),
+                },
+                new ApplicationDescription()
+                {
+                    ApplicationName = "SoftingOpcUaDemoServer1",
+                    ApplicationUri = "opc.tcp://localhost:51511/UA/DemoServer1",
+                    ApplicationType = ApplicationType.Server,
+                    ProductUri = "http://industrial.softing.com/OpcUaNetToolkit/OpcUaDemoServer1",
+                    DiscoveryUrls = new StringCollection(new List<string>(){
+                            "opc.tcp://localhost:51511/UA/DemoServer1"
+                    }
+                    ),
+                }
+
+
+
+            });
+
+            servers = serverDummy;
+
+            ResponseHeader responseHeader = new ResponseHeader();
+
+            responseHeader.Timestamp = DateTime.UtcNow;
+            responseHeader.RequestHandle = requestHeader.RequestHandle;
+            responseHeader.ServiceResult = new StatusCode(StatusCodes.Good);
+
+
+            return responseHeader;
+        }
+
+        public override ResponseHeader FindServersOnNetwork(RequestHeader requestHeader, uint startingRecordId, uint maxRecordsToReturn, StringCollection serverCapabilityFilter, out DateTime lastCounterResetTime, out ServerOnNetworkCollection servers)
+        {
+            return base.FindServersOnNetwork(requestHeader, startingRecordId, maxRecordsToReturn, serverCapabilityFilter, out lastCounterResetTime, out servers);
+        }
+
+        public override EndpointDescriptionCollection GetEndpoints()
+        {
+            return base.GetEndpoints();
+        }
+
+        public override ResponseHeader GetEndpoints(RequestHeader requestHeader, string endpointUrl, StringCollection localeIds, StringCollection profileUris, out EndpointDescriptionCollection endpoints)
+        {
+            endpoints = null;
+
+            //ValidateRequest(requestHeader);
+
+
+            // filter by profile.
+            IList<BaseAddress> baseAddresses = FilterByProfile(profileUris, BaseAddresses);
+
+            // get the descriptions.
+            endpoints = GetEndpointDescriptions(
+                endpointUrl,
+                baseAddresses,
+                localeIds);
+
+
+            return CreateResponse(requestHeader, StatusCodes.Good);
+        }
+
+        protected override void ValidateRequest(RequestHeader requestHeader)
+        {
+            base.ValidateRequest(requestHeader);
+        }
+
+
+        public override ResponseHeader RegisterServer2(RequestHeader requestHeader, RegisteredServer server, ExtensionObjectCollection discoveryConfiguration, out StatusCodeCollection configurationResults, out DiagnosticInfoCollection diagnosticInfos)
+        {
+            return base.RegisterServer2(requestHeader, server, discoveryConfiguration, out configurationResults, out diagnosticInfos);
+        }
+
+        public override ResponseHeader RegisterServer(RequestHeader requestHeader, RegisteredServer server)
+        {
+            return base.RegisterServer(requestHeader, server);
+        }
+
+        /// <summary>
+        /// Returns the endpoints that match the base addresss and endpoint url.
+        /// </summary>
+        protected EndpointDescriptionCollection GetEndpointDescriptions(
+            string endpointUrl,
+            IList<BaseAddress> baseAddresses,
+            StringCollection localeIds)
+        {
+            EndpointDescriptionCollection endpoints = null;
+
+            // parse the url provided by the client.
+            Uri parsedEndpointUrl = Utils.ParseUri(endpointUrl);
+
+            if (parsedEndpointUrl != null)
+            {
+                baseAddresses = FilterByEndpointUrl(parsedEndpointUrl, baseAddresses);
+            }
+
+            // check if nothing to do.
+            if (baseAddresses.Count != 0)
+            {
+                // localize the application name if requested.
+                LocalizedText applicationName = this.ServerDescription.ApplicationName;
+
+                // translate the application description.
+                ApplicationDescription application = TranslateApplicationDescription(
+                    parsedEndpointUrl,
+                    base.ServerDescription,
+                    baseAddresses,
+                    applicationName);
+
+                // translate the endpoint descriptions.
+                endpoints = TranslateEndpointDescriptions(
+                    parsedEndpointUrl,
+                    baseAddresses,
+                    this.Endpoints,
+                    application);
+            }
+
+            return endpoints;
+        }
+
+
+        #endregion
+
+        /// <summary>
+        /// Creates the endpoints and creates the hosts.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="bindingFactory">The binding factory.</param>
+        /// <param name="serverDescription">The server description.</param>
+        /// <param name="endpoints">The endpoints.</param>
+        /// <returns>
+        /// Returns IList of a host for a UA service which type is <seealso cref="ServiceHost"/>.
+        /// </returns>
+        protected override IList<Task> InitializeServiceHosts(
+            ApplicationConfiguration configuration,
+            out ApplicationDescription serverDescription,
+            out EndpointDescriptionCollection endpoints)
+        {
+            serverDescription = null;
+            endpoints = null;
+
+            Dictionary<string, Task> hosts = new Dictionary<string, Task>();
+
+            // ensure at least one security policy exists.
+            if (configuration.ServerConfiguration.SecurityPolicies.Count == 0)
+            {
+                configuration.ServerConfiguration.SecurityPolicies.Add(new ServerSecurityPolicy());
+            }
+
+            // ensure at least one user token policy exists.
+            if (configuration.ServerConfiguration.UserTokenPolicies.Count == 0)
+            {
+                UserTokenPolicy userTokenPolicy = new UserTokenPolicy();
+
+                userTokenPolicy.TokenType = UserTokenType.Anonymous;
+                userTokenPolicy.PolicyId = userTokenPolicy.TokenType.ToString();
+
+                configuration.ServerConfiguration.UserTokenPolicies.Add(userTokenPolicy);
+            }
+
+            // set server description.
+            serverDescription = new ApplicationDescription();
+
+            serverDescription.ApplicationUri = configuration.ApplicationUri;
+            serverDescription.ApplicationName = new LocalizedText("en-US", configuration.ApplicationName);
+            serverDescription.ApplicationType = configuration.ApplicationType;
+            serverDescription.ProductUri = configuration.ProductUri;
+            serverDescription.DiscoveryUrls = GetDiscoveryUrls();
+
+            endpoints = new EndpointDescriptionCollection();
+            IList<EndpointDescription> endpointsForHost = null;
+
+            // create UA TCP host.
+            endpointsForHost = CreateUaTcpServiceHost(
+                hosts,
+                configuration,
+                configuration.ServerConfiguration.BaseAddresses,
+                serverDescription,
+                configuration.ServerConfiguration.SecurityPolicies);
+
+            endpoints.InsertRange(0, endpointsForHost);
+
+            // create HTTPS host.
+#if !NO_HTTPS
+            endpointsForHost = CreateHttpsServiceHost(
+                hosts,
+                configuration,
+                configuration.ServerConfiguration.BaseAddresses,
+                serverDescription,
+                configuration.ServerConfiguration.SecurityPolicies);
+
+            endpoints.AddRange(endpointsForHost);
+#endif
+            return new List<Task>(hosts.Values);
+        }
+
+
+        protected override EndpointBase GetEndpointInstance(ServerBase server)
+        {
+            return new DiscoveryEndpoint(server);
+        }
+    }
+}
---
+++ LightLDS/Program.cs
@@ -0,0 +1,168 @@
+﻿using Opc.Ua;
+using Opc.Ua.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace LightLDS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ApplicationInstance application = new ApplicationInstance();
+
+            try
+            {
+
+                var applicationConfiguration = new ApplicationConfiguration();
+
+                applicationConfiguration.ApplicationName = "Light UA Local Discovery Server";
+                applicationConfiguration.ApplicationUri = "urn:localhost:K47S:LocalDiscoveryServer";
+                applicationConfiguration.ProductUri = "http://K47S/LocalDiscoveryServer";
+                applicationConfiguration.ApplicationType = ApplicationType.DiscoveryServer;
+
+                var securityConfiguration = new SecurityConfiguration();
+
+                securityConfiguration.ApplicationCertificate = new CertificateIdentifier()
+                {
+                    StoreType = @"Directory",
+                    StorePath = @"%CommonApplicationData%/LightLDS/PKI/own",
+                    SubjectName = "CN=Local Discovery Server, O=K47S, DC=localhost",
+                };
+
+                securityConfiguration.TrustedIssuerCertificates = new CertificateTrustList()
+                {
+                    StoreType = @"Directory",
+                    StorePath = @"%CommonApplicationData%/LightLDS/PKI/issuers",
+
+                };
+
+                securityConfiguration.TrustedPeerCertificates = new CertificateTrustList()
+                {
+                    StoreType = @"Directory",
+                    StorePath = @"%CommonApplicationData%/LightLDS/PKI/trusted",
+
+                };
+
+                securityConfiguration.RejectedCertificateStore = new CertificateTrustList()
+                {
+                    StoreType = @"Directory",
+                    StorePath = @"%CommonApplicationData%/LightLDS/PKI/rejected",
+
+                };
+
+                securityConfiguration.AutoAcceptUntrustedCertificates = true;
+                securityConfiguration.RejectSHA1SignedCertificates = false;
+                securityConfiguration.MinimumCertificateKeySize = 1024;
+
+
+
+                applicationConfiguration.SecurityConfiguration = securityConfiguration;
+
+
+                var serverConfiguration = new ServerConfiguration();
+
+                serverConfiguration.BaseAddresses = new StringCollection(new List<string>() { "opc.tcp://localhost:4840" });
+
+                serverConfiguration.SecurityPolicies = new ServerSecurityPolicyCollection(new List<ServerSecurityPolicy>() {
+
+                    new ServerSecurityPolicy()
+                    {
+                        SecurityMode = MessageSecurityMode.None,
+                        SecurityPolicyUri = "http://opcfoundation.org/UA/SecurityPolicy#None",
+                    }
+                });
+
+                serverConfiguration.UserTokenPolicies = new UserTokenPolicyCollection(new List<UserTokenPolicy>() {
+                    new UserTokenPolicy()
+                    {
+                        TokenType = UserTokenType.Anonymous,
+                        SecurityPolicyUri = "http://opcfoundation.org/UA/SecurityPolicy#None"
+                    }
+                });
+
+                serverConfiguration.DiagnosticsEnabled = true;
+                serverConfiguration.MaxSessionCount = 100;
+                serverConfiguration.MinSessionTimeout = 10000;
+                serverConfiguration.MaxSessionTimeout = 3600000;
+                serverConfiguration.MaxBrowseContinuationPoints = 10;
+                serverConfiguration.MaxQueryContinuationPoints = 10;
+                serverConfiguration.MaxHistoryContinuationPoints = 100;
+                serverConfiguration.MaxRequestAge = 600000;
+                serverConfiguration.MinPublishingInterval = 100;
+                serverConfiguration.MaxPublishingInterval = 3600000;
+                serverConfiguration.PublishingResolution = 100;
+                serverConfiguration.MaxSubscriptionLifetime = 3600000;
+                serverConfiguration.MaxMessageQueueSize = 100;
+                serverConfiguration.MaxNotificationQueueSize = 100;
+                serverConfiguration.MaxNotificationsPerPublish = 1000;
+                serverConfiguration.MinMetadataSamplingInterval = 1000;
+
+                serverConfiguration.RegistrationEndpoint = new EndpointDescription()
+                {
+                    EndpointUrl = "opc.tcp://localhost:4840",
+                    Server = new ApplicationDescription()
+                    {
+                        ApplicationUri = "opc.tcp://localhost:4840",
+                        ApplicationType = ApplicationType.DiscoveryServer,
+                        DiscoveryUrls = new StringCollection(new List<string>() { "opc.tcp://localhost:4840" })
+                    },
+                    SecurityMode = MessageSecurityMode.None
+                };
+
+                serverConfiguration.MaxRegistrationInterval = 0;
+
+                applicationConfiguration.ServerConfiguration = serverConfiguration;
+
+
+                var transportQuotaConfig = new TransportQuotas();
+
+
+                applicationConfiguration.TransportQuotas = transportQuotaConfig;
+
+
+                var traceConfiguration = new TraceConfiguration();
+
+                traceConfiguration.OutputFilePath = "%CommonApplicationData%/LightLDS/Logs/LightDiscoveryServer.log.txt";
+                traceConfiguration.DeleteOnLoad = true;
+
+                traceConfiguration.TraceMasks = 0x7FFFFFFF;
+
+
+
+                applicationConfiguration.TraceConfiguration = traceConfiguration;
+
+
+                application.ApplicationConfiguration = applicationConfiguration;
+
+
+                application.ApplicationConfiguration.Validate(ApplicationType.Server);
+
+                // check the application certificate.
+                application.CheckApplicationInstanceCertificate(false, 0).Wait();
+
+
+                var server = new DiscoveryServerInstance();
+
+                // start the server.
+                application.Start(server).Wait();
+                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+    
+
+    
+}
